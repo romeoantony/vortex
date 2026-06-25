0827edf8058c8dd3d4668ccfe634fdc299acb4bd
Akshay Antony <romeoantony1999@gmail.com>
2026-06-25 22:41:31 +0530

    Remove duration delay in SmoothScroll, use snappy lerp

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -4,8 +4,7 @@ import Lenis from '@studio-freight/lenis'
 export default function SmoothScroll({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.15, // Higher lerp = faster, less delay
       direction: 'vertical',
       gestureDirection: 'vertical',
       smooth: true,
