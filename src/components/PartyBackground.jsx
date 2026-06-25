03a972533a683a75a705e58b3d263f30a9c8cd61
Akshay Antony <romeoantony1999@gmail.com>
2026-06-26 00:29:51 +0530

    Replace party background spotlights with floating neon orbs to differentiate from homepage

--- src/components/PartyBackground.jsx
+++ src/components/PartyBackground.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-export default function PartyBackground({ imageSrc = "/assets/images/gatix_event.png" }) {
+export default function PartyBackground({ imageSrc }) {
   return (
     <div className="party-bg-wrapper">
-      <div className="hero__scanlines" style={{ zIndex: 2 }}></div>
-      <div className="hero__bg">
-        <img src={imageSrc} alt="Background" />
-        <div className="disco-lights">
-          <div className="spotlight spotlight-1"></div>
-          <div className="spotlight spotlight-2"></div>
-          <div className="spotlight spotlight-3"></div>
-          <div className="spotlight spotlight-4"></div>
+      {imageSrc && (
+        <div className="party-bg-image">
+          <img src={imageSrc} alt="Background" />
         </div>
-        <div className="hero__overlay" style={{ background: 'linear-gradient(to top, var(--bg) 0%, rgba(19,19,20,0.6) 100%)' }}></div>
+      )}
+      <div className="party-bg-overlay"></div>
+      
+      <div className="party-orbs">
+        <div className="orb orb-1"></div>
+        <div className="orb orb-2"></div>
+        <div className="orb orb-3"></div>
       </div>
+      
+      <div className="hero__scanlines" style={{ zIndex: 3, opacity: 0.15 }}></div>
     </div>
   )
 }
